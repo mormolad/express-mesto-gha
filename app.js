c6b42cff5b2974dc0ec70f11c9f29a0d883c9967
Perevalov Anton <mormolad@yandex.ru>
2023-08-03 00:36:37 +0700

    fist commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+// Слушаем 3000 порт
+const { PORT = 3000 } = process.env;
+
+const app = express();
+app.get("/", () => console.log("server start GET geting"));
+
+app.listen(PORT, () => {
+  console.log("server start");
+});
