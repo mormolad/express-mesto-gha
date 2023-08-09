a07de86f492d13fcdf890c501986bc5c90941949
mormolad <mormolad@yandex.ru>
2023-08-10 01:03:42 +0700

    delete like novalid2

--- controllers/cards.js
+++ controllers/cards.js
@@ -56,11 +56,11 @@ const deleteLike = (req, res) => {
     { new: true }
   )
     .then((card) => {
-      console.log();
-      if (!card) {
+      if (card) {
+        return res.status(200).send({ message: card.likes });
+      } else {
         return res.status(noFind.code).send({ message: noFind.message });
       }
-      return res.status(200).send({ message: card.likes });
     })
     .catch((err) => {
       console.log(err.valueType);
