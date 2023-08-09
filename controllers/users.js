4821a49cac26cbe819a7aa00ecfa53378cbb4df7
mormolad <mormolad@yandex.ru>
2023-08-09 17:17:45 +0700

    test

--- controllers/users.js
+++ controllers/users.js
@@ -59,11 +59,9 @@ const updateProfile = (req, res) => {
     { new: true }
   )
     .then((user) => {
-      console.log(!user);
       if (!user) {
         return res.status(noFind.code).send({ message: noFind.message });
       } else {
-        console.log(user);
         return res.status(200).send({ message: user });
       }
     })
