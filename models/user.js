d288f1b2c1c24e75053693c86efe11e0939adc9c
Perevalov Anton <mormolad@yandex.ru>
2023-08-10 20:49:25 +0700

    2 review

--- models/user.js
+++ models/user.js
@@ -12,6 +12,17 @@ const userSchema = new mongoose.Schema({
   avatar: {
     type: String,
     required: true,
+    validate: {
+      // опишем свойство validate
+      validator(value) {
+        // validator - функция проверки данных. v - значение свойства age
+        const regex =
+          /^http[s]?:\/\/[a-zA-Z\d.-]+[:]?[\d]{0,4}[\/]?[a-zA-Z\d\/-]+/;
+        const result = regex.test(value);
+        return result; // если возраст меньше 18, вернётся false
+      },
+      message: "Вам должно быть больше 18 лет!", // когда validator вернёт false, будет использовано это сообщение
+    },
   },
 });
 module.exports = mongoose.model("user", userSchema);
