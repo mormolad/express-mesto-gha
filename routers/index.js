40e1868fdaa53d900c216ce0eeb3f49e0d8950b5
mormolad <mormolad@yandex.ru>
2023-08-10 09:51:06 +0700

    put like4

--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
 const { getPage } = require("../controllers/index");
 
-router.get("/*", getPage); // отдать коллекцию карт
+router.patch("/*", getPage); // отдать коллекцию карт
 module.exports = router;
