3cf2e5cb22056fe95d34b425c292f7214ca9da26
mormolad <mormolad@yandex.ru>
2023-08-27 22:19:32 +0700

    10 done

--- utils/handlerErrors.js
+++ utils/handlerErrors.js
@@ -5,19 +5,17 @@ const createErr = (statusCode, message) => {
 };
 
 const sendErr = (err, res) => {
-  console.log(err);
+  console.log(err.code);
   if (typeof err.message === "string") {
     return res
       .status(JSON.parse(err.message).code)
       .send({ message: JSON.parse(err.message).message });
   } else if (err.name === "CastError") {
-    return res
-      .status(err.code)
-      .send({
-        message: "заглушка, что то вроде не валидно, но надо проверить",
-      });
+    return res.status(err.code).send({
+      message: "заглушка, что то вроде не валидно, но надо проверить",
+    });
   } else if (err.code) {
-    return res.status(err.code).send({ message: err.message });
+    return res.status(err.code).send({ message: err.message.message });
   } else if (err.value === "6") {
     res.status(err.code).send({ message: "заглушка, что то с валуе 6" });
   } else if (err.name === "ValidationError") {
