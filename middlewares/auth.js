4b075c11de99a3d5dcc9996058cd17dfa6441de5
mormolad <paspean@mail.ru>
2023-09-03 11:03:17 +0700

    test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,13 +1,13 @@
 const { isJWT, getPayload } = require("../utils/jwt");
 const { CustomeError } = require("../utils/handlerErrors");
-const { noAuth } = require("../errors");
+const { errLogin } = require("../errors");
 const auth = (req, res, next) => {
   if (req.headers.authorization) {
     isJWT(req.headers.authorization)
       ? (req.user = { _id: getPayload(req.headers.authorization)._id })
-      : next(new CustomeError(noAuth.code, noAuth.message));
+      : next(new CustomeError(errLogin.code, errLogin.message));
   } else {
-    next(new CustomeError(noAuth.code, noAuth.message));
+    next(new CustomeError(errLogin.code, errLogin.message));
   }
   next();
 };
