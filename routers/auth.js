eb7210a8ba2447fe43ea12a05e535cdf7a07972d
mormolad <paspean@mail.ru>
2023-09-04 12:40:49 +0700

    test joi

--- routers/auth.js
+++ routers/auth.js
@@ -6,7 +6,11 @@ routerAuth.post(
   "/signup",
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required().min(2).max(30),
+      email: Joi.string()
+        .email({ minDomainSegments: 2 })
+        .required()
+        .min(2)
+        .max(30),
       password: Joi.string().required().min(2),
     }),
   }),
@@ -16,7 +20,11 @@ routerAuth.post(
   "/signin",
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string().required().min(2).max(30),
+      email: Joi.string()
+        .email({ minDomainSegments: 2 })
+        .required()
+        .min(2)
+        .max(30),
       password: Joi.string().required().min(2),
     }),
   }),
