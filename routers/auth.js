1392e6901a3af46a4d3f364df6df280003f2ac92
mormolad <mormolad@yandex.ru>
2023-09-07 22:41:07 +0700

    test joi signup

--- routers/auth.js
+++ routers/auth.js
@@ -6,11 +6,7 @@ routerAuth.post(
   "/signup",
   celebrate({
     body: Joi.object().keys({
-      email: Joi.string()
-        .email({ minDomainSegments: 2 })
-        .required()
-        .min(2)
-        .max(30),
+      email: Joi.string().email({ minDomainSegments: 2 }).required(),
       password: Joi.string().required().min(2),
       about: Joi.string().min(2).max(30),
       name: Joi.string().min(2).max(30),
