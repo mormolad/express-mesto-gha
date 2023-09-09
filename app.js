e843f3f7a7b75ab732bd153cd9934f149a76a527
Perevalov Anton <mormolad@yandex.ru>
2023-09-09 19:38:15 +0700

    debug for 2 review

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const helmet = require("helmet");
 const mongoose = require("mongoose");
 const path = require("path");
 const routerUser = require("./routers/users");
@@ -25,7 +26,8 @@ mongoose
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(helmet());
+app.disable("x-powered-by");
 app.use(routerAuth);
 app.use(auth);
 app.use(routerUser);
@@ -36,25 +38,3 @@ app.use(sendError);
 app.listen(PORT, () => {
   console.log(`server start, listen port: ${PORT}`);
 });
-
-// function sendError(err, req, res, next) {
-//    console.log("блок поиска ошибок, код ошибки = ", err, "/n конец ошибки");
-//   if (err.statusCode) {
-//     return res.status(err.statusCode).send({ message: err.message });
-//   } else if (err.name === "CastError") {
-//     return res.status(err.code).send({
-//       message: "заглушка, что то вроде не валидно, но надо проверить",
-//     });
-//   } else if (err.code) {
-//     return res
-//       .status(409)
-//       .send({ message: "пользователь с таким емайлом существует" });
-//   } else if (err.value === "6") {
-//     res.status(403).send({ message: "заглушка, что то с валуе 6" });
-//   } else if (err.name === "ValidationError") {
-//     console.log(err);
-//     res.status(403).send({ message: "заглушка, что то не завалидировалось" });
-//   } else {
-//     res.status(500).send({ message: err });
-//   }
-// }
