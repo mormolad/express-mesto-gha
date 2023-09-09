68a650da17e2025a05d87dbb1a1a01fa7c2cf1e3
Perevalov Anton <mormolad@yandex.ru>
2023-09-09 15:07:07 +0700

    test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,13 +3,17 @@ const { CustomeError } = require("../utils/handlerErrors");
 const { errLogin, noAuth } = require("../errors");
 const auth = (req, res, next) => {
   //console.log("wtf");
-  if (req.headers.authorization) {
-    isJWT(req.headers.authorization)
-      ? (req.user = { _id: getPayload(req.headers.authorization)._id })
-      : next(new CustomeError(errLogin.code, errLogin.message));
-  } else {
+  if (
+    !req.headers.authorization ||
+    !req.headers.authorization.startsWith("Bearer ")
+  ) {
     next(new CustomeError(noAuth.code, noAuth.message));
   }
+  const token = req.headers.authorization.replace("Bearer ", "");
+  isJWT(token)
+    ? (req.user = { _id: getPayload(token)._id })
+    : next(new CustomeError(errLogin.code, errLogin.message));
+
   next();
 };
 
